f8986596bf5fd2aa0b24342e65767f90dd3b1510
Bruno <brunorodrigues.silveira@gmail.com>
2020-05-25 20:41:08 -0300

    Iniciando Lista

---
+++ app/src/main/java/com/example/speedbuy/Lojas.java
@@ -0,0 +1,32 @@
+package com.example.speedbuy;
+
+public class Lojas {
+    private String nome;
+    private String foto;
+    private String estrela;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getEstrela() {
+        return estrela;
+    }
+
+    public void setEstrela(String estrela) {
+        this.estrela = estrela;
+    }
+}
