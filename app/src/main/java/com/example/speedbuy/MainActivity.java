af48216883064d7b740d5aa986743ca80c430719
Bruno <brunorodrigues.silveira@gmail.com>
2020-06-05 14:06:54 -0300

    BOttom Navigation

--- app/src/main/java/com/example/speedbuy/MainActivity.java
+++ app/src/main/java/com/example/speedbuy/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.speedbuy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 
@@ -12,5 +16,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
+
+        private BottomNavigationView.OnNavigationItemReselectedListener bottomNavigation = new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.home:
+                        list_view listViewHome = new list_view();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.linear_list, listViewHome).commit();
+                        return true;
+                }
+                return false;
+            }
+        };
     }
 }
